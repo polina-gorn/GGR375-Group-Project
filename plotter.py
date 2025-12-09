f8e4573e2f6c74cee537d7a7508d80df63937b2f
Samantha Kyle <samanthalkyle@gmail.com>
2025-12-09 16:05:38 -0500

    added morans i plot function and multichoropleth function

--- plotter.py
+++ plotter.py
@@ -10,6 +10,49 @@ streetcar_lines_2025 = None
 bus_lines_2015 = None
 bus_lines_2025 = None
 
+def plot_local_moran(local_moran_obj, gdf, title):
+    ax = local_moran_obj.plot(gdf, legend = True, legend_kwds = {'loc': 'lower right'})
+    ax.set_title(title)
+    ax.set_axis_off()
+    return ax.figure, ax
+
+def plot_choropleths(gdf: gpd.GeoDataFrame, suptitle: str, titles, columns,
+                     schemes = None, cmap = 'GnBu') -> tuple:
+    '''
+    Plot multiple maps in a ROW
+
+    gdf: GeoDataFrame to plot - mandatory
+    title: title of figure - mandatory
+    column: column with which to apply cmap - mandatory
+    scheme: quantiles unless otherwise stated
+    cmap: GnBu unless otherwise stated
+    figsize: (10,10) unless otherwise stated
+    '''
+    
+    if len(columns) != len(titles):
+        print('Lengths of lists do not match!')
+        return None
+    if schemes == None:
+        schemes = ['quantiles' * len(columns)]
+
+    ncols = len(titles)
+
+    figsize = (10 * ncols, 8)
+
+    fig, axes = plt.subplots(nrows = 1, ncols = ncols, figsize = figsize, squeeze=False)
+    fig.suptitle(suptitle)
+
+    for i in range(ncols):
+        gdf.plot(ax = axes[0,i], column = columns[i], cmap = cmap, scheme = schemes[i], legend = True,
+                 legend_kwds={'loc': 'lower right', 'title': schemes[i]})
+        axes[0, i].set_title(titles[i])
+        axes[0, i].set_axis_off()
+        
+    if ncols == 1:
+        return fig, axes[0, 0]
+    else:
+        return fig, axes
+
 
 def plot_choropleth(gdf: gpd.GeoDataFrame, title: str, column: str,
                      scheme = 'quantiles', cmap = 'GnBu', figsize = (10,10)) -> tuple:
@@ -25,11 +68,13 @@ def plot_choropleth(gdf: gpd.GeoDataFrame, title: str, column: str,
     '''
     fig, ax = plt.subplots(figsize = figsize)
 
-    gdf.plot(ax = ax, column = column, cmap = cmap, scheme = scheme, legend = True)
+    gdf.plot(ax=ax,column = column, cmap = cmap, scheme = scheme, legend = True,
+             legend_kwds={'loc': 'lower right'})
     ax.set_title(title)
     ax.set_axis_off()
-
+        
     return fig, ax
+
 
 def plot_subway_lines(fig, ax, year: str):
     if year == '2015' or year == 2015:
